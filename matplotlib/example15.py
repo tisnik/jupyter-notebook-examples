a21543901b90eeb25e614b9b2aa52805b27ee791
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-23 07:11:44 +0200

    Script for example #15

---
+++ matplotlib/example15.py
@@ -0,0 +1,15 @@
+# Jupyter Notebook
+#
+# Patnáctý demonstrační příklad:
+# - jednoduchý histogram
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# náhodné hodnoty
+y = np.random.normal(0, 0.1, 10000)
+
+plt.hist(y, bins=30, range=None, normed=True)
+
+# zobrazení grafu
+plt.show()
