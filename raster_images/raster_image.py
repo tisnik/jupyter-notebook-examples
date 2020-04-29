c44701e1fc700b37691e8dbf41da9b64889ea8bf
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-04-29 10:15:02 +0200

    Script to create raster image

---
+++ raster_images/raster_image.py
@@ -0,0 +1,27 @@
+
+# coding: utf-8
+
+# In[36]:
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+# In[37]:
+
+
+raster = np.zeros(shape=(450, 450, 3), dtype=np.uint8)
+
+
+# In[38]:
+
+
+plt.imshow(raster)
+
+
+# In[39]:
+
+
+plt.show()
+
