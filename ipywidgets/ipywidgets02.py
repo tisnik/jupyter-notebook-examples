e18560b99b4559856ff6193e567b90ec0d4baaaf
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-03 07:55:47 +0200

    2nd notebook converted into Python script

---
+++ ipywidgets/ipywidgets02.py
@@ -0,0 +1,32 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import micropip
+
+
+# In[2]:
+
+
+await micropip.install("ipywidgets")
+
+
+# In[3]:
+
+
+import ipywidgets
+
+
+# In[5]:
+
+
+slider = ipywidgets.IntSlider()
+
+
+# In[6]:
+
+
+slider
+
