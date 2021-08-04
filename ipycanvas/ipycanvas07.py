e36f0798116d511b71fb1ed6d17bb3b5f7c4c8d0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-04 08:37:19 +0200

    7th notebook converted into Python script

---
+++ ipycanvas/ipycanvas07.py
@@ -0,0 +1,61 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# příprava knihovny určené pro instalaci dalších knihoven do prostředí prohlížeče
+import micropip
+
+
+# In[2]:
+
+
+# instalace knihovny ipycanvas do prostředí prohlížeče
+await micropip.install("ipycanvas")
+
+
+# In[15]:
+
+
+# nyní je již možné provést import nově nainstalované knihovny
+import ipycanvas
+
+
+# In[69]:
+
+
+# vytvoření kreslicího plátna
+canvas = ipycanvas.Canvas(width=320, height=240)
+
+
+# In[70]:
+
+
+# vložení kreslicího plátna na plochu diáře
+canvas
+
+
+# In[71]:
+
+
+gradient = canvas.create_linear_gradient(0, 0, 320,0,
+    [
+        (0, 'red'),
+        (1 / 6, 'orange'),
+        (2 / 6, 'yellow'),
+        (3 / 6, 'green'),
+        (4 / 6, 'blue'),
+        (5 / 6, '#4B0082'),
+        (1, 'violet')
+    ]
+)
+
+
+# In[72]:
+
+
+# obdélník
+canvas.fill_style = gradient
+canvas.fill_rect(0, 0, 320, 240)
+
